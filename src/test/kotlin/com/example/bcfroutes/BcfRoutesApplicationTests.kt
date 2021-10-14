504d7ffc7dad9c33ee0f87e5d4d60c15a9ef6667
aokilipa <aokilipa@gmail.com>
2021-10-14 17:04:51 +0300

    initial commit

---
+++ src/test/kotlin/com/example/bcfroutes/BcfRoutesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bcfroutes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BcfRoutesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
